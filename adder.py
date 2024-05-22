c2a5233744796d42ed09d788db1e5aca18173513
Dunne-Willows <Michael.Willows@ons.gov.uk>
2024-05-22 13:32:53 +0100

    Updating adder
    Added documentation
    Added test cases

--- adder.py
+++ adder.py
@@ -5,8 +5,18 @@ Spyder Editor
 This is a temporary script file.
 """
 
+# Function to add some numbers
+
 def add(a,b,c):
     ans = a+b+c
     return ans
 
+# Print the answer to demonstrate the functionality
 print(add(3,7,2))
+
+# Test case
+# 1,2,3 -> 6
+if add(1,2,3) == 6:
+    print("Test passed")
+else:
+    print("Test failed")

d54e97aa4db3464be216c89f85f5c1dd24769ee2
Dunne-Willows <Michael.Willows@ons.gov.uk>
2024-05-22 15:38:39 +0100

    Updated Adder and Multi
    Added tests to multi and added user input functionality to adder

--- adder.py
+++ adder.py
@@ -7,10 +7,15 @@ This is a temporary script file.
 
 # Function to add some numbers
 # Better defaults
-def add(a=0,b=0,c=0, show = False):
+def add(a=0,b=0,c=0, show = False, user = False):
+    if user:
+        a = int(input("Enter a: "))
+        b = int(input("Enter b: "))
+        c = int(input("Enter c: "))
+        
     ans = a+b+c
     if show:
-        print(f'{a}+{b}+{c}={ans}')
+        print(f'{a} + {b} + {c} = {ans}')
     return ans
 
 # Print the answer to demonstrate the functionality
@@ -23,3 +28,4 @@ if add(1,2,3) == 6:
 else:
     print("Test failed")
 
+add(show = True, user = True)
--- multiply.py
+++ multiply.py
@@ -3,5 +3,16 @@ def multi(a,b):
 	ans = a*b
 	return ans
 
-print(multi(4, 6))
 
+
+# Test case
+# 3*9 -> 27
+if multi(3,9) == 27:
+    print("Test #1 Passed")
+
+# Test case
+# three*4 -> nameerror
+try:
+    multi(three, 4)
+except NameError:
+    print("Test #2 Passed")
